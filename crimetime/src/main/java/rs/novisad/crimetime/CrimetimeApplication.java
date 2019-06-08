6b42c4d3064f9d7ef7b8a8f73cc31cb44366526a
Kosta Kupresak <kupresak98@gmail.com>
2019-06-08 11:01:14 +0200

    Initial

---
+++ crimetime/src/main/java/rs/novisad/crimetime/CrimetimeApplication.java
@@ -0,0 +1,13 @@
+package rs.novisad.crimetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimetimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimetimeApplication.class, args);
+	}
+
+}
---
+++ crimetime/src/test/java/rs/novisad/crimetime/CrimetimeApplicationTests.java
@@ -0,0 +1,16 @@
+package rs.novisad.crimetime;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrimetimeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
