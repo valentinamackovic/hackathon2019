c180d9fb0b97fc36756363604434ce70f251a778
Boris <sobota365@gmail.com>
2019-06-09 14:46:41 +0200

    legend on map

--- crimetime/src/main/resources/static/js/index.js
+++ crimetime/src/main/resources/static/js/index.js
@@ -55,6 +55,34 @@ function getClusters(){
             best5.forEach(function(entry) {
                 bestEl.append('<li>' + entry.name + '</li>');
             });
+            //-----LEGEND
+
+            function getColor(d) {
+                return  d > red   ? 'red':
+                        d > orange   ? 'orange' :
+                        d > yellow   ? 'yellow' :
+                                    'green';
+            }
+
+            var legend = L.control({position: 'bottomright'});
+
+            legend.onAdd = function (map) {
+
+            var div = L.DomUtil.create('div', 'info legend'),
+            grades = [green, yellow, orange,red],
+            labels = [];
+
+            // loop through our density intervals and generate a label with a colored square for each interval
+            for (var i = 0; i < grades.length; i++) {
+                div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+    return div;
+};
+
+legend.addTo(map);
 
         }
     });
